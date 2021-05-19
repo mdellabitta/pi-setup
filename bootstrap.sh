5b6a70ed2c01a596536433afc5dcb1ad995a2699
Michael Della Bitta <mdellabitta@gmail.com>
2021-05-19 01:24:53 +0100

    updated bootstrap for debian

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,67 @@
-#!/bin/sh
+#!/usr/bin/bash
 
-set -x
+# The purpose of this script is to get the box set up for running the
+# ansible playbooks in the repo. We're installing the latest ansible
+# using pip, and pip and a recent python using pyenv.
 
-sudo apt install -y ansible
-sudo ansible-galaxy collection install community.general
+# Script is written assuming Debian Buster. May work elsewhere.
+
+set -euxo pipefail
+
+sudo apt update
+
+# Most of these dependencies are here to get python builds working.
+
+sudo apt install -y \
+	git \
+	make \
+	build-essential \
+	libssl-dev \
+	zlib1g-dev \
+	libbz2-dev \
+	libreadline-dev \
+	libsqlite3-dev \
+	wget \
+	curl \
+	llvm \
+	libncursesw5-dev \
+	xz-utils \
+	tk-dev \
+	libxml2-dev \
+	libxmlsec1-dev \
+	libffi-dev \
+	liblzma-dev \
+	rustc
+
+if ! [[ -d ~/pi-setup ]]
+then
+	git clone https://github.com/mdellabitta/pi-setup.git
+fi
+
+if ! [[ -d ~/.pyenv ]]
+then
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	cd ~/.pyenv && src/configure && make -C src; cd ~
+fi
+
+if [ $(grep -q pyenv ~/.profile) ]
+then
+	echo 'Installing pyenv into .profile'
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' > profile.tmp
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> profile.tmp
+	echo 'eval "$(pyenv init --path)"' >> profile.tmp
+	cat .profile >> profile.tmp
+	mv .profile .profile.bak.$(timestamp=$(date +%s))
+	mv profile.tmp .profile
+	export PYENV_ROOT="$HOME/.pyenv"
+	export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init --path)"
+fi
+
+pyenv install -s 3.9.5
+cd ~/pi-setup/ansible
+pyenv local 3.9.5
+python -m venv venv
+pip install ansible
+ansible-galaxy collection install community.general
+cd ~
