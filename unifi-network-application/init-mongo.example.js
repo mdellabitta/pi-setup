a5166c2230527cb6268175e1c34c6d2c49b468e6
Michael Della Bitta <mdellabitta@tutanota.com>
2024-06-18 10:52:13 -0400

    transitioned to linuxserver's new container for unifi

---
+++ unifi-network-application/init-mongo.example.js
@@ -0,0 +1,17 @@
+db
+	.getSiblingDB("unifi")
+	.createUser({
+		user: "unifi", 
+		pwd: "CHANGEME", 
+		roles: [
+			{
+				role: "dbOwner", 
+				db: "unifi"
+			}, 
+			{ 
+				role: "dbOwner", 
+				db: "unifi_stat" 
+			}
+		]
+	});
+   
